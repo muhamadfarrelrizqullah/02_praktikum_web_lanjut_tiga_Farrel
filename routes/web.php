9b3ac0650898a137b65357761c807c13d4d42cc9
muhamadfarrelrizqullah <farrelswag27@gmail.com>
2023-03-01 18:23:30 +0700

    Commit Percobaan3

---
+++ routes/web.php
@@ -0,0 +1,59 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactUsController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/home', function (){
+    return redirect('https://www.educastudio.com');
+});
+
+Route::prefix('category')->group(function () {
+    Route::get('/marbel-edu-games', function () {
+        return redirect('https://www.educastudio.com/category/marbel-edu-games');
+    });
+    Route::get('/marbel-and-friends-kids-games', function () {
+        return redirect('https://www.educastudio.com/category/marbel-and-friends-kids-games');
+    });
+    Route::get('/riri-story-books', function () {
+        return redirect('https://www.educastudio.com/category/riri-story-books-animations');
+    });
+    Route::get('/kolak-kids-songs', function () {
+        return redirect('https://www.educastudio.com/category/kolak-kids-songs');
+    });
+});
+
+Route::get('/news/{title}', function ($title) {
+    return redirect('https://www.educastudio.com/news/'.$title);
+});
+
+Route::prefix('program')->group(function () {
+    Route::get('/karir', function () {
+        return redirect('https://www.educastudio.com/program/karir');
+    });
+    Route::get('/magang', function () {
+        return redirect('https://www.educastudio.com/program/magang');
+    });
+    Route::get('/kunjungan-industri', function () {
+        return redirect('https://www.educastudio.com/program/kunjungan-industri');
+    });
+});
+
+Route::get('/about-us', function () {
+    return redirect('https://www.educastudio.com/about-us');
+});
+
+Route::resource('contact-us', ContactUsController::class)->only([
+    'index'
+]);
+
